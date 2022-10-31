464b1eed2468e4436f870d11d7ae178125b5bffd
brewlabs-code <garlic@brewlabs.info>
2022-10-31 17:48:45 +1000

    Add image to background

--- components/layout/PageWrapper.tsx
+++ components/layout/PageWrapper.tsx
@@ -1,14 +1,31 @@
 import { ReactNode } from "react";
+import Image from "next/future/image";
 import { motion } from "framer-motion";
 import { useRouter } from "next/router";
+import Bubbles from "../animations/Bubbles";
 
 const PageWrapper = ({ children }: { children: ReactNode }) => {
   const router = useRouter();
 
   const variants = {
-    hidden: { opacity: 0, x: 0, y: 500 },
-    enter: { opacity: 1, x: 0, y: 0 },
-    exit: { opacity: 0, x: 0, y: -500 },
+    hidden: {
+      opacity: 0,
+      x: 0,
+      y: 500,
+      scale: 1.1,
+    },
+    enter: {
+      opacity: 1,
+      x: 0,
+      y: 0,
+      scale: 1,
+    },
+    exit: {
+      opacity: 0,
+      x: 0,
+      y: -500,
+      scale: 0.95,
+    },
   };
 
   const variantsHome = {
@@ -23,11 +40,20 @@ const PageWrapper = ({ children }: { children: ReactNode }) => {
       initial="hidden"
       animate="enter"
       exit="exit"
-      transition={{ type: "linear" }}
       variants={router.pathname === "/" ? variantsHome : variants}
-      className="flex-1 relative z-0 focus:outline-none xl:order-last"
+      className="relative z-0 flex-1 focus:outline-none xl:order-last"
     >
-      <div className="h-full">{children}</div>
+      <Bubbles />
+      <Image
+        className="fixed top-0 -right-44 opacity-50"
+        src="./images/blur-indigo.png"
+        alt=""
+        width={567}
+        height={567}
+        unoptimized
+        priority
+      />
+      {children}
     </motion.div>
   );
 };
