a38a118613689a7ce5a884b64311fff05e38da50
brewlabs-code <garlic@brewlabs.info>
2022-10-31 17:55:05 +1000

    Update _document.tsx

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -8,7 +8,7 @@ export default function Document() {
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link
-          href="https://fonts.googleapis.com/css2?family=Questrial&display=swap"
+          href="https://fonts.googleapis.com/css2?family=Questrial&family=Caveat&display=swap"
           rel="stylesheet"
         />
       </Head>
