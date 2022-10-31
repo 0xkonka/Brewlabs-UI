ac2277f76d2e41b07529f6244c25287fcd748552
brewlabs-code <garlic@brewlabs.info>
2022-10-31 17:49:28 +1000

    Create connect wallet button

--- components/ConnectWallet.tsx
+++ components/ConnectWallet.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { setGlobalState } from "../state";
-import { BeakerIcon } from "@heroicons/react/outline";
+import { BeakerIcon } from "@heroicons/react/24/outline";
 import { useAccount, useConnect, useDisconnect } from "wagmi";
 import { InjectedConnector } from "wagmi/connectors/injected";
 
@@ -19,14 +19,14 @@ const ConnectWallet = () => {
   if (!mounted) return null;
 
   return (
-    <div className="flex-shrink-0 flex p-4 border-t border-gray-200 dark:border-gray-800">
+    <div className="flex flex-shrink-0 border-t border-gray-200 p-4 dark:border-gray-800">
       {!isConnected ? (
         <button
           onClick={() => connect()}
-          className="flex-shrink-0 w-full group block"
+          className="group block w-full flex-shrink-0"
         >
-          <div className="flex items-center animate-pulse">
-            <div className="border-2 border-dark rounded-full p-2">
+          <div className="flex animate-pulse items-center">
+            <div className="rounded-full border-2 border-dark p-2">
               <BeakerIcon className="inline-block h-6 w-6 rounded-full" />
             </div>
             <div className="ml-3">
@@ -42,17 +42,17 @@ const ConnectWallet = () => {
       ) : (
         <button
           onClick={() => setGlobalState("userSidebarOpen", true)}
-          className="flex-shrink-0 w-full group block"
+          className="group block w-full flex-shrink-0"
         >
           <div className="flex items-center">
-            <div className="border-2 border-dark dark:border-brand rounded-full p-2">
+            <div className="rounded-full border-2 border-dark p-2 dark:border-brand">
               <BeakerIcon className="inline-block h-6 w-6 rounded-full" />
             </div>
             <div className="ml-3 overflow-hidden">
-              <p className="text-sm font-medium truncate text-gray-700 hover:text-gray-900 dark:text-gray-500 dark:hover:text-gray-100">
+              <p className="truncate text-sm font-medium text-gray-700 hover:text-gray-900 dark:text-gray-500 dark:hover:text-gray-100">
                 {isLoading ? "..." : address}
               </p>
-              <p className="text-xs text-left font-medium text-gray-500 hover:text-gray-700 dark:text-gray-400 dark:hover:text-gray-100">
+              <p className="text-left text-xs font-medium text-gray-500 hover:text-gray-700 dark:text-gray-400 dark:hover:text-gray-100">
                 Connected
               </p>
             </div>
