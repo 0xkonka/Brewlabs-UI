5381abfaa141ab569c11f9b1c439e5ad1002de22
brewlabs-code <garlic@brewlabs.info>
2022-10-31 17:30:17 +1000

    Update pages to use standard markup

--- pages/farms.tsx
+++ pages/farms.tsx
@@ -3,6 +3,7 @@ import Head from "next/head";
 import PageHeader from "../components/PageHeader";
 import Container from "../components/layout/Container";
 import PageWrapper from "../components/layout/PageWrapper";
+import WordHighlight from "../components/text/WordHighlight";
 
 const Farms: NextPage = () => {
   return (
@@ -15,10 +16,18 @@ const Farms: NextPage = () => {
         />
       </Head>
 
-      <PageHeader />
+      <PageHeader
+        title={
+          <>
+            Discover the power of <WordHighlight content="farming" /> staked
+            liquidity.
+          </>
+        }
+        summary="Stake liquidity tokens in our Brewlabs farming platform for passive income."
+      />
 
       <Container>
-        <div className="h-80 w-full border -mt-8 dark:border-gray-900 rounded-lg flex justify-center items-center bg-zinc-900 shadow-lg">
+        <div className="-mt-8 flex h-80 w-full items-center justify-center rounded-lg border bg-zinc-900 shadow-lg dark:border-gray-900">
           <h3 className="font-brand text-xl">Farms go here</h3>
         </div>
       </Container>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,10 @@
 import type { NextPage } from "next";
 import Head from "next/head";
 import CardVideo from "../components/cards/CardVideo";
-
 import Footer from "../components/Footer";
 import Container from "../components/layout/Container";
 import PageWrapper from "../components/layout/PageWrapper";
-import Navbar from "../components/Navbar";
 import PageHero from "../components/PageHero";
-import styles from "../styles/Home.module.css";
 
 const Home: NextPage = () => (
   <PageWrapper>
@@ -16,19 +13,17 @@ const Home: NextPage = () => (
       <meta name="description" content="Earn with Brewlabs" />
     </Head>
 
-    {/* <Navbar /> */}
-
     <PageHero
       pageTitle="Welcome to Brewlabs"
       pageIntro="Make use of the Brewlabs constructor to create tax free liquidity tokens."
     />
 
-    <main className="min-h-screen mt-16">
+    <main className="mt-16 min-h-screen">
       <section>
         <Container className="grid grid-cols-1 gap-6 sm:grid-cols-2 md:gap-y-10 lg:grid-cols-3">
-          <CardVideo cardId="1" />
-          <CardVideo cardId="2" />
-          <CardVideo cardId="3" />
+          <CardVideo cardId="1" youtubeId="ZJgpQ9EpVvo" />
+          <CardVideo cardId="2" youtubeId="ZJgpQ9EpVvo" />
+          <CardVideo cardId="3" youtubeId="ZJgpQ9EpVvo" />
         </Container>
       </section>
     </main>
