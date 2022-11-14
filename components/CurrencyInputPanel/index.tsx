06c605d23f423e35a751161103118334acdc5740
Smart Ninja <james.blockchain.dev@gmail.com>
2022-11-14 08:51:58 -0500

    add currency select

--- components/CurrencyInputPanel/index.tsx
+++ components/CurrencyInputPanel/index.tsx
@@ -1,4 +1,5 @@
 import { ChevronDownIcon } from "@heroicons/react/24/outline";
+import { TOKENS } from "config/constants/tokens";
 import Card from "../../views/Swap/components/Card";
 import NumericalInput from "./NumericalInput";
 
@@ -11,21 +12,28 @@ const CurrencyInputPanel = ({ label }: CurrencyInputPanelProps) => {
     <Card>
       <div className="ml-6">
         <div>{label}</div>
-        <div className="mt-1 flex justify-between">
+        <div className="flex justify-between mt-1">
           <div>
             <NumericalInput value="0.00" />
-            <div className="text-xs">0.00 USD</div>
+            <div className="text-xs opacity-40">0.00 USD</div>
           </div>
           <div className="flex justify-end" style={{ minWidth: "170px" }}>
             <div className="flex">
               <div>
-                <div className="mr-1 text-2xl">ETH</div>
-                <div className="flex items-center">
+                <select
+                  id="currency"
+                  name="currency"
+                  className="w-full py-0 text-2xl bg-transparent border-transparent"
+                >
+                  <option className="text-gray-500">ETH</option>
+                  <option className="text-gray-500">BREW</option>
+                  <option className="text-gray-500">GROVE</option>
+                </select>
+                <div className="flex items-center ml-4">
                   <div className="mr-2 text-xs">24.21 ETH</div>
-                  <img src="/images/explorer/etherscan.png" alt="" className="h-2 w-2" />
+                  <img src="/images/explorer/etherscan.png" alt="" className="w-2 h-2" />
                 </div>
               </div>
-              <ChevronDownIcon className="mt-1 h-5 w-5 dark:text-brand" />
             </div>
           </div>
         </div>
