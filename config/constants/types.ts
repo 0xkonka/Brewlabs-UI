e385a9a1cb9569cee7ebf97899ddea023a9a35ee
brewlabs-code <garlic@brewlabs.info>
2022-11-17 16:42:10 +1000

    Types formatted and adding 0 as chain ID

--- config/constants/types.ts
+++ config/constants/types.ts
@@ -1,38 +1,38 @@
 import { ChainId, Currency } from "@brewlabs/sdk";
 
 export interface SerializedToken {
-  chainId: ChainId
-  isNative: boolean
-  isToken: boolean
-  address?: string
-  decimals: number
-  symbol?: string
-  name?: string
-  projectLink?: string
+  chainId: ChainId;
+  isNative: boolean;
+  isToken: boolean;
+  address?: string;
+  decimals: number;
+  symbol?: string;
+  name?: string;
+  projectLink?: string;
 }
 
 export enum PoolCategory {
-  'COMMUNITY' = 'Community',
-  'CORE' = 'Core',
-  'BINANCE' = 'Binance', // Pools using native BNB behave differently than pools using a token
-  'AUTO' = 'Auto',
-  'LOCKUP' = 'Lockup',
-  'LOCKUP_V2' = 'Lockup_V2',
-  'MULTI' = 'Multi',
-  'MULTI_LOCKUP' = 'Multi_Lockup',
+  "COMMUNITY" = "Community",
+  "CORE" = "Core",
+  "BINANCE" = "Binance", // Pools using native BNB behave differently than pools using a token
+  "AUTO" = "Auto",
+  "LOCKUP" = "Lockup",
+  "LOCKUP_V2" = "Lockup_V2",
+  "MULTI" = "Multi",
+  "MULTI_LOCKUP" = "Multi_Lockup",
 }
 
 export enum Version {
-  'V1' = 'V1',
-  'V2' = 'V2',
-  'V3' = 'V3',
+  "V1" = "V1",
+  "V2" = "V2",
+  "V3" = "V3",
 }
 
 export enum AppId {
-  PANCAKESWAP = 'pancakeswap',
-  APESWAP = 'apeswap',
-  KNIGHT = 'knight',
-  SUSHISWAP = 'sushiswap',
+  PANCAKESWAP = "pancakeswap",
+  APESWAP = "apeswap",
+  KNIGHT = "knight",
+  SUSHISWAP = "sushiswap",
 }
 
 export enum Chef {
@@ -41,83 +41,83 @@ export enum Chef {
 }
 
 interface FarmConfigBaseProps {
-  pid: number
-  farmId?: number
-  poolId?: number
-  v1pid?: number
-  chainId?: ChainId
-  version?: Version
-  lpSymbol: string
-  lpDecimals?: number
-  lpAddress: string
-  contractAddress?: string
-  multiplier?: string
-  isCommunity?: boolean
-  lpManager?: string
-  enableEmergencyWithdraw?: boolean
-  isServiceFee?: boolean
-  compound?: boolean
-  compoundRelection?: boolean
-  unverified?: boolean
-  featured?: boolean
-  isFinished?: boolean
+  pid: number;
+  farmId?: number;
+  poolId?: number;
+  v1pid?: number;
+  chainId?: ChainId;
+  version?: Version;
+  lpSymbol: string;
+  lpDecimals?: number;
+  lpAddress: string;
+  contractAddress?: string;
+  multiplier?: string;
+  isCommunity?: boolean;
+  lpManager?: string;
+  enableEmergencyWithdraw?: boolean;
+  isServiceFee?: boolean;
+  compound?: boolean;
+  compoundRelection?: boolean;
+  unverified?: boolean;
+  featured?: boolean;
+  isFinished?: boolean;
   dual?: {
-    rewardPerBlock: number
-    earnLabel: string
-    endBlock: number
-  },
-  externalSwap?: string
+    rewardPerBlock: number;
+    earnLabel: string;
+    endBlock: number;
+  };
+  externalSwap?: string;
 }
 
 export interface SerializedFarmConfig extends FarmConfigBaseProps {
-  apr?: number,
-  token: SerializedToken
-  quoteToken: SerializedToken
-  earningToken?: SerializedToken
-  reflectionToken?: SerializedToken
+  apr?: number;
+  token: SerializedToken;
+  quoteToken: SerializedToken;
+  earningToken?: SerializedToken;
+  reflectionToken?: SerializedToken;
 }
 
 export interface DeserializedFarmConfig extends FarmConfigBaseProps {
-  token: Currency
-  quoteToken: Currency
-  earningToken?: Currency
-  reflectionToken?: Currency
+  token: Currency;
+  quoteToken: Currency;
+  earningToken?: Currency;
+  reflectionToken?: Currency;
 }
 
 interface PoolConfigBaseProps {
-  sousId: number
-  chainId: ChainId
-  contractAddress: string
-  poolCategory: PoolCategory
-  tokenPerBlock: string
-  sortOrder?: number
-  version?: Version
-  harvest?: boolean
-  noCompound?: boolean
-  reflection?: boolean
-  noReflectionCompound?: boolean
-  lockup?: number
-  isServiceFee?: boolean
-  isFinished?: boolean
-  migration?: boolean
-  unverified?: boolean
-  featured?: boolean
-  enableEmergencyWithdraw?: boolean
-  forceEndblock?: number
-  headerSuffix?: string
-  externalSwap?: string
+  sousId: number;
+  chainId: ChainId;
+  contractAddress: string;
+  poolCategory: PoolCategory;
+  tokenPerBlock: string;
+  sortOrder?: number;
+  version?: Version;
+  harvest?: boolean;
+  noCompound?: boolean;
+  reflection?: boolean;
+  noReflectionCompound?: boolean;
+  lockup?: number;
+  isServiceFee?: boolean;
+  isFinished?: boolean;
+  migration?: boolean;
+  unverified?: boolean;
+  featured?: boolean;
+  enableEmergencyWithdraw?: boolean;
+  forceEndblock?: number;
+  headerSuffix?: string;
+  externalSwap?: string;
 }
 
 export interface SerializedPoolConfig extends PoolConfigBaseProps {
-  earningToken: SerializedToken
-  stakingToken: SerializedToken
-  reflectionTokens?: SerializedToken[]
+  earningToken: SerializedToken;
+  stakingToken: SerializedToken;
+  reflectionTokens?: SerializedToken[];
 }
 
 export interface DeserializedPoolConfig extends PoolConfigBaseProps {
-  earningToken: Currency
-  stakingToken: Currency
-  reflectionTokens?: Currency[]
+  earningToken: Currency;
+  stakingToken: Currency;
+  reflectionTokens?: Currency[];
 }
 
 export type PageMeta = {
@@ -127,28 +127,28 @@ export type PageMeta = {
 };
 
 export type NetworkConfig = {
-  id: ChainId;
+  id: ChainId | 0;
   name: string;
   image: string;
 };
 
-type LinkOfTextAndLink = string | { text: string; url: string }
+type LinkOfTextAndLink = string | { text: string; url: string };
 type DeviceLink = {
-  desktop?: LinkOfTextAndLink
-  mobile?: LinkOfTextAndLink
-}
-type LinkOfDevice = string | DeviceLink
+  desktop?: LinkOfTextAndLink;
+  mobile?: LinkOfTextAndLink;
+};
+type LinkOfDevice = string | DeviceLink;
 
 export type WalletConfig<T = unknown> = {
-  id: string
-  title: string
-  description: string
-  icon: string
-  connectorId: T
-  deepLink?: string
-  installed?: boolean
-  guide?: LinkOfDevice
-  downloadLink?: LinkOfDevice
-  mobileOnly?: boolean
-  qrCode?: () => Promise<string>
-}
+  id: string;
+  title: string;
+  description: string;
+  icon: string;
+  connectorId: T;
+  deepLink?: string;
+  installed?: boolean;
+  guide?: LinkOfDevice;
+  downloadLink?: LinkOfDevice;
+  mobileOnly?: boolean;
+  qrCode?: () => Promise<string>;
+};
