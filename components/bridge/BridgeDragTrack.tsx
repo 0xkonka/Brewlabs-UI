39f93bd2a515195a7ee121ad17da75600bd3869f
brewlabs-code <garlic@brewlabs.info>
2022-11-17 16:40:07 +1000

    Update drag track to use new data type

--- components/bridge/BridgeDragTrack.tsx
+++ components/bridge/BridgeDragTrack.tsx
@@ -16,25 +16,20 @@ const BridgeDragTrack = ({ setLockingFn }: { setLockingFn: Dispatch<SetStateActi
     }
   };
 
-  const handleDragEnd = () => {
-    setLockingFn(false);
-  };
-
   return (
-    <div className="relative z-10 col-span-3 -my-6 flex flex-col items-center justify-between sm:-mx-6 sm:flex-row">
-      <div className="absolute left-0 top-0 h-16 w-16 rounded-full border-2 border-dotted border-gray-400 bg-gray-200 dark:border-gray-700 dark:bg-slate-800"></div>
+    <div className="z-10 col-span-3 -my-6 hidden flex-col items-center justify-between sm:relative sm:-mx-6 sm:flex sm:flex-row">
+      <div className="absolute h-16 w-16 rounded-full border-2 border-dotted border-gray-400 bg-gray-200 dark:border-gray-700 dark:bg-slate-800"></div>
 
       <motion.div
         drag="x"
         whileDrag={{ scale: 1.2, zIndex: 90 }}
         whileHover={{ scale: 1.2 }}
         dragSnapToOrigin
-        onDragEnd={() => handleDragEnd()}
         onDrag={(event, info) => handleDrag(info)}
         dragConstraints={{ left: 0, right: 200 }}
         className="rounded-ful relative h-16 w-16 shrink-0 overflow-hidden bg-cover bg-no-repeat hover:cursor-grab"
         style={{
-          backgroundImage: `url('${NetworkOptions.find((network) => network.name === networkFrom)?.image}')`,
+          backgroundImage: `url('${networkFrom.image}')`,
         }}
       ></motion.div>
 
@@ -42,14 +37,14 @@ const BridgeDragTrack = ({ setLockingFn }: { setLockingFn: Dispatch<SetStateActi
         initial={{ opacity: 0 }}
         animate={{ opacity: 1 }}
         transition={{ delay: 4, duration: 2 }}
-        className="absolute -top-16 left-16 w-36 -rotate-12 font-script text-xl"
+        className="absolute left-16 -top-16 hidden w-36 -rotate-12 font-script text-xl sm:visible"
       >
         <p>Drag to complete the transaction.</p>
       </motion.div>
 
-      <div className="-z-10 h-36 w-1 animate-pulse border-r-4 border-dotted border-gray-300 dark:border-gray-700 sm:h-1 sm:w-full sm:border-t-4" />
+      <div className="z-10 h-1 w-full animate-pulse border-r-4 border-dotted border-gray-300 dark:border-gray-700 sm:border-t-4" />
 
-      {networkFrom !== "No network selected" && (
+      {networkFrom.name !== "No network selected" && (
         <div className="slide-x absolute left-14">
           <ChevronDoubleRightIcon className="h-6 w-6 text-gray-500" />
         </div>
@@ -58,7 +53,7 @@ const BridgeDragTrack = ({ setLockingFn }: { setLockingFn: Dispatch<SetStateActi
       <div
         className="h-16 w-16 shrink-0 overflow-hidden rounded-full border-2 border-dotted border-gray-400 bg-gray-200 bg-cover bg-no-repeat dark:border-gray-700 dark:bg-slate-800"
         style={{
-          backgroundImage: `url('${NetworkOptions.find((network) => network.name === networkTo)?.image}')`,
+          backgroundImage: `url('${networkTo.image}')`,
         }}
       ></div>
     </div>
