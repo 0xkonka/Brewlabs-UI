1529ff2e91516c8a0627614a159818c1c25c7a77
Smart Ninja <james.blockchain.dev@gmail.com>
2022-11-18 03:50:32 -0500

    switch network on swap

--- views/Swap/components/ChainSelect.tsx
+++ views/Swap/components/ChainSelect.tsx
@@ -1,11 +1,27 @@
+import { useSupportedNetworks } from "hooks/useSupportedNetworks";
+import { useSwitchNetwork } from "hooks/useSwitchNetwork";
+import { useNetwork } from "wagmi";
 import Card from "./Card";
 
 const ChainSelect = () => {
+  const { chain } = useNetwork();
+  const supportedNetworks = useSupportedNetworks();
+  const { switchNetwork } = useSwitchNetwork();
+
   return (
     <Card>
-      <select id="chain" name="chain" className="w-full border-transparent bg-transparent px-0 py-0">
-        <option className="text-gray-500">Ethereum</option>
-        <option className="text-gray-500">BNB Smart Chain</option>
+      <select
+        id="chain"
+        name="chain"
+        value={chain?.id}
+        onChange={(e) => switchNetwork(+e.target.value)}
+        className="w-full px-0 py-0 bg-transparent border-transparent"
+      >
+        {supportedNetworks.map((network) => (
+          <option value={network.id} className="text-gray-500">
+            {network.name}
+          </option>
+        ))}
       </select>
     </Card>
   );
