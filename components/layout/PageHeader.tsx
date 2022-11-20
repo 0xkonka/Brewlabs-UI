346561a5f761085f7e852a73cb0ec4b4c228a86f
brewlabs-code <garlic@brewlabs.info>
2022-11-20 17:44:38 +1000

    Clean up page header

--- components/layout/PageHeader.tsx
+++ components/layout/PageHeader.tsx
@@ -1,6 +1,5 @@
 import { ReactNode, useLayoutEffect, useRef, useState } from "react";
 import clsx from "clsx";
-import Image from "next/future/image";
 import Container from "./Container";
 
 // TIP: If you want words to be highlighted use JSX in the prop
@@ -34,27 +33,20 @@ const PageHeader = ({ title, summary, children }: PageHeaderProps) => {
   };
 
   return (
-    <section
-      className={clsx(
-        outOfView ? "animate__animated animate__fadeOut animate__faster" : "animate__animated animate__fadeIn"
-      )}
-    >
+    <section>
       <Container className="pt-20 pb-16">
-        <header ref={scrollRef} className="max-w-md pr-24 font-brand sm:pr-0">
+        <header
+          ref={scrollRef}
+          className={clsx(
+            "max-w-md pr-24 font-brand sm:pr-0",
+            outOfView ? "animate__animated animate__fadeOut animate__faster" : "animate__animated animate__fadeIn"
+          )}
+        >
           <h1 className="text-3xl font-extrabold text-slate-700 dark:text-slate-400 sm:text-4xl">{title}</h1>
           {summary && <p className="mt-4 text-slate-800">{summary}</p>}
         </header>
         {children}
       </Container>
-      <Image
-        className="absolute bottom-0 w-full dark:hidden"
-        src="./images/wave.svg"
-        alt=""
-        width={567}
-        height={567}
-        unoptimized
-        priority
-      />
     </section>
   );
 };
