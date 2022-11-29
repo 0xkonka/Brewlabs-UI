5fdb228a4aec7ac660313ea93835e82d1cbbbfcd
brewlabs-code <garlic@brewlabs.info>
2022-11-29 18:24:31 +1000

    Add new responsive styles
    Earlier breakpoint

--- components/cards/CryptoCard.tsx
+++ components/cards/CryptoCard.tsx
@@ -46,7 +46,7 @@ const CryptoCard = ({ id, title, tokenPrice, modal, active, network, children }:
         )}
       >
         <div className="h-72 rounded-3xl border-t border-slate-100 bg-gray-50 shadow-lg shadow-indigo-500/20 dark:border-slate-600 dark:bg-zinc-900">
-          <div className="p-10">
+          <div className="p-6">
             <header className="text-center text-gray-700 dark:text-gray-500">
               <h4 className="text-2xl">{title}</h4>
               <div className="mx-auto mt-4 flex items-center justify-center">
@@ -69,7 +69,7 @@ const CryptoCard = ({ id, title, tokenPrice, modal, active, network, children }:
                   <ChevronDownIcon className="h-6 w-6 pr-2" />
                 </button>
                 <div className="tooltip" data-tip="Transfer fee: 0.10%">
-                <InformationCircleIcon className="ml-1 h-6 w-6 cursor-pointer" />
+                  <InformationCircleIcon className="ml-1 h-6 w-6 cursor-pointer" />
                 </div>
               </div>
             </header>
