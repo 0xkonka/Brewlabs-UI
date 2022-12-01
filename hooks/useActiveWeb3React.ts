bfd953f4202528270d688e52379b47945587e1a0
brainstormk <mverse@brewlabs.info>
2022-12-01 11:20:53 +0800

    Update useActiveWeb3React.ts

--- hooks/useActiveWeb3React.ts
+++ hooks/useActiveWeb3React.ts
@@ -1,14 +1,19 @@
-import { useAccount, useProvider } from "wagmi";
+import { useAccount, useNetwork, useProvider } from "wagmi";
 import { useActiveChainId } from "./useActiveChainId";
 
 export default function useActiveWeb3React() {
+  const { chain } = useNetwork()
   const { chainId } = useActiveChainId();
   const provider = useProvider({ chainId });
-  const { address: account } = useAccount();
+  const { address: account, connector, isConnected, isConnecting } = useAccount();
 
   return {
+    account,
     chainId,
+    chain,
+    connector,
+    isConnected,
+    isConnecting,
     library: provider,
-    account: account,
   };
 }
