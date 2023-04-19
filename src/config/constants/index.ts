e4866eb698b91ca62c60c06203ecb53c05921aed
brainstormk <109654392+brainstormk@users.noreply.github.com>
2023-04-19 16:21:37 +0800

    Call price histories for index per 30 seconds

--- src/config/constants/index.ts
+++ src/config/constants/index.ts
@@ -5,6 +5,7 @@ import { BIG_TEN } from "utils/bigNumber";
 import { tokens } from "./tokens";
 
 export const FAST_INTERVAL = 10000;
+export const MEDIUM_INTERVAL = 30000;
 export const SLOW_INTERVAL = 60000;
 export const BIGSLOW_INTERVAL = 60000 * 2;
 export const DAY_INTERVAL = 60000 * 24 * 60;
