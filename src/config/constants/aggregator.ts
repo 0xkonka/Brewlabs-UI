0d2c43e71b05b0cfa9608270b32a2970e6271848
DevSCNinja <james.blockchain.dev@gmail.com>
2023-05-16 02:57:48 -0400

    add pcs v3, uni v3 to aggregator

--- src/config/constants/aggregator.ts
+++ src/config/constants/aggregator.ts
@@ -1,22 +1,38 @@
 export const Adapters = {
-    1: {},
-    56: {
-       "0xA31c745DC9D61D7840622e4Df490bAF44dF1Fc5B": {
-            name: "Apeswap",
-            logo: "https://raw.githubusercontent.com/trustwallet/assets/master/dapps/apeswap.finance.png",
-       },
-       "0x7F360b8a59bfb414692e8b4B57cedf1556c620AD": {
-            name: "Babyswap",
-            logo: "https://raw.githubusercontent.com/trustwallet/assets/master/dapps/babyswap.finance.png",
-       },
-       "0x3Be8C9E5130285478F143EE09091Aa6cbba1D256": {
-            name: "Pancakeswap",
-            logo: "https://raw.githubusercontent.com/trustwallet/assets/master/dapps/exchange.pancakeswap.finance.png",
-       },
-       "0x463fDFbdCBD55E83701C377d89b39Bbb7E0DFE54": {
-            name: "Sushiswap",
-            logo: "https://raw.githubusercontent.com/trustwallet/assets/master/dapps/app.sushi.com.png",
-       }
+  1: [],
+  56: [
+    {
+      address: "0xA31c745DC9D61D7840622e4Df490bAF44dF1Fc5B",
+      name: "Apeswap",
+      logo: "https://raw.githubusercontent.com/trustwallet/assets/master/dapps/apeswap.finance.png",
     },
-    97: {}
-}
+    {
+      address: "0x7F360b8a59bfb414692e8b4B57cedf1556c620AD",
+      name: "Babyswap",
+      logo: "https://raw.githubusercontent.com/trustwallet/assets/master/dapps/babyswap.finance.png",
+    },
+    {
+      address: "0x3Be8C9E5130285478F143EE09091Aa6cbba1D256",
+      name: "Pancakeswap",
+      logo: "https://raw.githubusercontent.com/trustwallet/assets/master/dapps/exchange.pancakeswap.finance.png",
+    },
+    {
+      address: "0x463fDFbdCBD55E83701C377d89b39Bbb7E0DFE54",
+      name: "Sushiswap",
+      logo: "https://raw.githubusercontent.com/trustwallet/assets/master/dapps/app.sushi.com.png",
+    },
+    {
+      address: "0x372Ba972B7337773d6040953De8aeEa72181Cb7d",
+      name: "Pancakeswap",
+      logo: "https://raw.githubusercontent.com/trustwallet/assets/master/dapps/exchange.pancakeswap.finance.png",
+      meta: "v3",
+    },
+    {
+      address: "0xbf46bE5EFc78234d34BFF0BF6dD6D33AeC66989a",
+      name: "Uniswap",
+      logo: "https://raw.githubusercontent.com/trustwallet/assets/master/dapps/app.uniswap.org.png",
+      meta: "v3",
+    },
+  ],
+  97: [],
+};
