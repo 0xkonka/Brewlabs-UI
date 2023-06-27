b6ec34c99adf211d941c69a0ca5829211826518e
sparklingcooldev <okamipeindev@gmail.com>
2023-06-27 17:46:04 -0400

    update soon and stablecoins

---
+++ src/components/Soon/index.tsx
@@ -0,0 +1,9 @@
+const Soon = ({ className }: { className?: string }) => (
+  <div
+    className={`${className} absolute -right-3 -top-2 z-10 flex h-4 w-10 items-center justify-center rounded-[30px] bg-primary font-brand text-xs font-bold text-black`}
+  >
+    Soon
+  </div>
+);
+
+export default Soon;
