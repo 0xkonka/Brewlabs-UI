e53e8e8a7d491b076e049e8ee22e25c10654a8dd
brainstormk <109654392+brainstormk@users.noreply.github.com>
2023-11-24 10:47:38 +0800

    update pair card

--- src/views/tradingPairs/PairCard.tsx
+++ src/views/tradingPairs/PairCard.tsx
@@ -72,7 +72,7 @@ export default function PairCard({ pair, setSelectedPair }) {
           {isLoading ? <SkeletonComponent /> : `$${numberWithCommas(data.tvl.toFixed(2))}`}
         </div>
         <div className={`${width[6]} `}>
-          {isLoading || !data.feesCollected24h ? (
+          {isLoading || data.feesCollected24h === undefined ? (
             <SkeletonComponent />
           ) : (
             `$${numberWithCommas(data.feesCollected24h.toFixed(2))}`
@@ -133,7 +133,7 @@ export default function PairCard({ pair, setSelectedPair }) {
         <div className="flex flex-wrap justify-between">
           <div className="mr-4 mt-2">
             Fees Collected:{" "}
-            {isLoading || !data.feesCollected24h ? (
+            {isLoading || data.feesCollected24h === undefined ? (
               <SkeletonComponent />
             ) : (
               `$${numberWithCommas(data.feesCollected24h.toFixed(2))}`
