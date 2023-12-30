1987148bdd976529c612ea77f9a9ec008387893a
brewlabs-code <garlic@brewlabs.info>
2023-12-30 11:27:57 +1000

    Remove the alert notice

--- src/components/layout/PageWrapper.tsx
+++ src/components/layout/PageWrapper.tsx
@@ -1,12 +1,11 @@
 import { ReactNode } from "react";
 import PageMeta from "./PageMeta";
-import AlertNotice from "@components/AlertNotice";
 
 const PageWrapper = ({ children }: { children: ReactNode }) => {
   return (
     <>
       <PageMeta />
-      <AlertNotice />
+      {/* <AlertNotice /> */}
       <div id="page_wrapper" className="relative z-0 min-h-screen w-full flex-1 xl:order-last">
         {children}
       </div>
