bc1f8cec19e91fd8039a3e6a4b94a584a15f21b4
garlic-brewlabs <88916999+garlic-brewlabs@users.noreply.github.com>
2024-01-21 13:06:09 +1000

    Update prop type

--- src/contexts/wagmi/src/hooks/useSignMessage.ts
+++ src/contexts/wagmi/src/hooks/useSignMessage.ts
@@ -1,24 +1,23 @@
-import { SignMessageArgs } from '@wagmi/core'
-import { useCallback } from 'react'
-import { useAccount, useSignMessage as useSignMessageWagmi } from 'wagmi'
+import { useCallback } from "react";
+import { useAccount, useSignMessage as useSignMessageWagmi } from "wagmi";
 
 export function useSignMessage() {
-  const { address, connector } = useAccount()
-  const { signMessageAsync: sign } = useSignMessageWagmi()
+  const { address, connector } = useAccount();
+  const { signMessageAsync: sign } = useSignMessageWagmi();
 
   return {
     signMessageAsync: useCallback(
-      async (args: SignMessageArgs) => {
-        if (connector?.id === 'bsc' && window.BinanceChain && address) {
-          const res = await window.BinanceChain.bnbSign?.(address, args.message as string)
+      async (args: { message: string }) => {
+        if (connector?.id === "bsc" && window.BinanceChain && address) {
+          const res = await window.BinanceChain.bnbSign?.(address, args.message as string);
           if (res) {
-            return res.signature
+            return res.signature;
           }
-          return null
+          return null;
         }
-        return sign(args)
+        return sign(args);
       },
-      [address, connector?.id, sign],
+      [address, connector?.id, sign]
     ),
-  }
+  };
 }
