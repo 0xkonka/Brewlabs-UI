7db9b5e233a9c826acac035fbe9ba4cd64327408
0xkonka <8722069+0xkonka@users.noreply.github.com>
2024-02-01 11:31:47 +0700

    Update index.tsx
    imported icon for button

--- src/views/swap/index.tsx
+++ src/views/swap/index.tsx
@@ -1,7 +1,7 @@
 import PageHeader from "components/layout/PageHeader";
 import Container from "components/layout/Container";
 import PageWrapper from "components/layout/PageWrapper";
-
+import { DocumentTextIcon } from "@heroicons/react/24/outline";
 import SwapBoard from "./SwapBoard";
 import { DocumentTextIcon } from "@heroicons/react/24/outline";
 import WordHighlight from "@components/text/WordHighlight";
