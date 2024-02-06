a7a72edf9c137ce51074868c9f7de6f31a8edb89
chainpioneer1 <pandaworld1985@outlook.com>
2024-02-06 16:38:04 +0200

    add farms on arbitrum one chain

--- src/config/constants/networks.ts
+++ src/config/constants/networks.ts
@@ -14,6 +14,7 @@ export const PAGE_SUPPORTED_CHAINS: { [key: string]: ChainId[] } = {
     ChainId.AVALANCHE,
     ChainId.CRONOS,
     ChainId.BRISE,
+    ChainId.ARBITRUM,
   ],
   staking: [
     ChainId.ETHEREUM,
