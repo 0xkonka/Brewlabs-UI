0ceefaada5e16f58e4cb0dd713145a7e5417a06d
garlic-brewlabs <88916999+garlic-brewlabs@users.noreply.github.com>
2024-04-15 17:32:37 +1000

    Better placement of component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,19 @@
 import type { NextPage } from "next";
 
 import Footer from "@components/layout/Footer";
-
 import PageWrapper from "@components/layout/PageWrapper";
 import PageHero from "@components/layout/PageHero";
-import FeaturePlug from "@components/FeaturePlug";
-import VideoSection from "@components/VideoSection";
+
+import FeaturePlug from "@components/homex/FeaturePlug";
+import VideoSection from "@components/homex/VideoSection";
+import FindProject from "@components/homex/FindProject";
 
 const Home: NextPage = () => (
   <PageWrapper>
     <PageHero />
 
     <main className="min-h-screen">
+      <FindProject />
       <VideoSection />
       <FeaturePlug />
     </main>
