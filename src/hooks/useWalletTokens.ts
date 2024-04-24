bea8779c0e975317625c737a1496c68c6b9c8892
garlic-brewlabs <88916999+garlic-brewlabs@users.noreply.github.com>
2024-04-24 15:17:39 +1000

    Add type def for returned value

--- src/hooks/useWalletTokens.ts
+++ src/hooks/useWalletTokens.ts
@@ -1,8 +1,23 @@
 import { useState, useEffect, useCallback } from "react";
 import axios from "axios";
 
+type WalletCurrency = {
+  balance: string;
+  decimals: number;
+  logo: string;
+  name: string;
+  percentage_relative_to_total_supply: number;
+  possible_spam: boolean;
+  symbol: string;
+  thumbnail: string;
+  token_address: string;
+  total_supply: string;
+  total_supply_formatted: string;
+  verified_contract: boolean;
+};
+
 const useWalletTokens = (walletAddress: string, chain: string) => {
-  const [tokens, setTokens] = useState([]);
+  const [tokens, setTokens] = useState<WalletCurrency[]>();
 
   const fetchTokens = useCallback(async () => {
     if (!chain || !walletAddress) {
