963a97443ec461c016af3ad180b1a3549924725d
garlic-brewlabs <88916999+garlic-brewlabs@users.noreply.github.com>
2024-05-01 14:01:54 +1000

    Adding Certik verification notice

--- src/pages/deployer/deploy-pool.tsx
+++ src/pages/deployer/deploy-pool.tsx
@@ -1,25 +1,45 @@
 import Container from "components/layout/Container";
 import PageWrapper from "components/layout/PageWrapper";
 import PageHeader from "components/layout/PageHeader";
-import WordHighlight from "components/text/WordHighlight";
 import PoolDeployer from "components/productDeployer/PoolDeployer";
+import { HoverCard, HoverCardContent, HoverCardTrigger } from "@components/ui/hover-card";
 
 export default function DeployToken() {
   return (
     <PageWrapper>
       <PageHeader
-        title={
-          <>
-            <WordHighlight content="Deploy" /> a new staking pool
-          </>
-        }
+        title="Deploy a new staking pool"
         summary={
           <>
             Welcome to the Brewlabs product deployer wizard.<br></br> Using this wizard will allow you to deploy a range
             of Brewlabs products.
           </>
         }
-      ></PageHeader>
+      >
+        <div className="mt-8">
+          <HoverCard>
+            <HoverCardTrigger asChild>
+              <img src="/images/certik-logo-white.svg" alt="Certik logo" className="h-auto w-24" />
+            </HoverCardTrigger>
+            <HoverCardContent className="w-80">
+              <div className="flex justify-between space-x-4">
+                <div className="space-y-1">
+                  <h4 className="text-sm font-semibold">All our deploy contracts are CertiK verified</h4>
+                  <p className="my-6 text-sm text-gray-400">
+                    We take security seriously, all our deploy contracts are verified by CertiK. This means you can be
+                    confident that the contract you are deploying is secure and safe.
+                  </p>
+                  <div className="flex items-center pt-2">
+                    <a href="#" target="_blank" className="text-xs text-muted-foreground">
+                      View audit here
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </HoverCardContent>
+          </HoverCard>
+        </div>
+      </PageHeader>
 
       <Container className="animate__animated animate__fadeIn animate__faster pb-4 font-brand">
         <PoolDeployer />
--- src/pages/deployer/deploy-token.tsx
+++ src/pages/deployer/deploy-token.tsx
@@ -1,18 +1,17 @@
 import Container from "components/layout/Container";
 import PageWrapper from "components/layout/PageWrapper";
 import PageHeader from "components/layout/PageHeader";
-import WordHighlight from "components/text/WordHighlight";
 import TokenDeployer from "components/productDeployer/TokenDeployer";
+import { HoverCard, HoverCardContent, HoverCardTrigger } from "@components/ui/hover-card";
+
+import { ArrowRight } from "@radix-ui/react-icons";
+import { ChevronsRightIcon } from "lucide-react";
 
 export default function DeployToken() {
   return (
     <PageWrapper>
       <PageHeader
-        title={
-          <>
-            <WordHighlight content="Deploy" /> a new token contract
-          </>
-        }
+        title="Deploy a new token contract"
         summary={
           <>
             Welcome to the Brewlabs product deployer wizard.<br></br> Using this wizard will allow you to deploy a range
@@ -20,10 +19,41 @@ export default function DeployToken() {
           </>
         }
       >
-        <p className="mt-4 text-xs">
+        <p className="mt-4 text-xs text-gray-400">
           *Staking pools, Yield farms and Indexes will also deploy to the Brewlabs directory, you can find the latest
           pools easily be filtering with the “New” category.
         </p>
+        <div className="mt-8">
+          <HoverCard>
+            <HoverCardTrigger asChild>
+              <div className="flex w-fit items-center gap-2">
+                <span className="text-xs font-thin tracking-[0.25rem]">VERIFIED BY - </span>
+                <img src="/images/certik-logo-white.svg" alt="Certik logo" className="h-auto w-24" />
+              </div>
+            </HoverCardTrigger>
+            <HoverCardContent className="w-80">
+              <div className="flex justify-between space-x-4">
+                <div className="space-y-4">
+                  <h4 className="text-sm font-semibold">All our deploy contracts are CertiK verified</h4>
+                  <p className="my-6 text-sm text-gray-400">
+                    We take security seriously, all our deploy contracts are verified by{" "}
+                    <a href="https://www.certik.com/" target="_blank">
+                      CertiK
+                    </a>
+                    . This means you can be confident that the contract you are deploying is secure and safe.
+                  </p>
+                  <a
+                    href="#"
+                    target="_blank"
+                    className="flex items-end gap-1 pt-2 text-xs text-muted-foreground underline"
+                  >
+                    View audit here <ChevronsRightIcon className="h-4 w-4" />
+                  </a>
+                </div>
+              </div>
+            </HoverCardContent>
+          </HoverCard>
+        </div>
       </PageHeader>
 
       <Container className="pb-4 font-brand">
