da533c07c96bb8abcf898af06134a0d25b29a78f
garlic-brewlabs <88916999+garlic-brewlabs@users.noreply.github.com>
2024-05-02 10:03:24 +1000

    Refactor countdown - unmount

--- src/components/CountDown/index.tsx
+++ src/components/CountDown/index.tsx
@@ -1,30 +1,37 @@
-/* eslint-disable react-hooks/exhaustive-deps */
-
 import { useEffect, useState } from "react";
 
 let timerid = null;
+
 const CountDown = ({ time, finishedText }: { time: number; finishedText?: string }) => {
   const [timeText, setTimeText] = useState("");
   const formatNumber = (date) => {
     return ("0" + date.toString()).slice(-2);
   };
-  const formatTime = () => {
-    const _time = Math.floor((time - Date.now()) / 1000);
-    if (_time <= 0) {
-      setTimeText(finishedText);
-      return;
-    }
-    let d = Math.floor(_time / 3600 / 24);
-    let h = Math.floor((_time % 86400) / 3600);
-    let m = Math.floor(((_time % 86400) % 3600) / 60);
-    let s = _time % 60;
-    setTimeText(formatNumber(d) + ":" + formatNumber(h) + ":" + formatNumber(m) + ":" + formatNumber(s));
-  };
+
   useEffect(() => {
+    const formatTime = () => {
+      const _time = Math.floor((time - Date.now()) / 1000);
+      if (_time <= 0) {
+        setTimeText(finishedText);
+        return;
+      }
+      let d = Math.floor(_time / 3600 / 24);
+      let h = Math.floor((_time % 86400) / 3600);
+      let m = Math.floor(((_time % 86400) % 3600) / 60);
+      let s = _time % 60;
+
+      setTimeText(formatNumber(d) + ":" + formatNumber(h) + ":" + formatNumber(m) + ":" + formatNumber(s));
+    };
+
     timerid = setInterval(() => {
       formatTime();
     }, 1000);
-  }, [time]);
+
+    return () => {
+      clearInterval(timerid);
+    };
+  }, [finishedText, time]);
+
   return <div className="leading-none">{timeText}</div>;
 };
 
