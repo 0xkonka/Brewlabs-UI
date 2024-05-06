f1fea66d1d1c9abd37e46f643dccc33160ecb795
garlic-brewlabs <88916999+garlic-brewlabs@users.noreply.github.com>
2024-05-06 23:04:45 +1000

    NFT image getting - needs more work

---
+++ src/utils/getNftImage.ts
@@ -0,0 +1,31 @@
+import type { EvmNft } from "@moralisweb3/common-evm-utils";
+
+function removeAfterIpfs(url) {
+  const ipfsIndex = url.indexOf("/ipfs/");
+  if (ipfsIndex !== -1) {
+    // if '/ipfs/' not found, return the original URL
+    return url.slice(0, ipfsIndex + "/ipfs/".length);
+  }
+  return url;
+}
+
+const createIPFSURL = (tokenUri, imagePath) => {
+  const trimmedUrl = removeAfterIpfs(tokenUri);
+  const x = imagePath?.replace("ipfs://", trimmedUrl);
+
+  return x;
+};
+
+export const getNftImage = (nft: EvmNft): string => {
+  // If brewlabs NFT
+  if (nft.name.includes("Brewlabs Flask NFT")) {
+    return "/images/nfts/brewlabs-flask-nfts/brewlabs-flask-common.png";
+  }
+
+  // Try that - Metadata image
+  if (nft.metadata && "image" in nft.metadata) {
+    return createIPFSURL(nft.tokenUri, nft.metadata.image);
+  }
+
+  return "/images/nfts/default.png";
+};
