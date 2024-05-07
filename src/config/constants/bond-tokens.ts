c267c8789ebe8b98c7607c6d80f7a386773c3f7e
garlic-brewlabs <88916999+garlic-brewlabs@users.noreply.github.com>
2024-05-07 23:23:02 +1000

    Add type to better place

--- src/config/constants/bond-tokens.ts
+++ src/config/constants/bond-tokens.ts
@@ -384,3 +384,10 @@ export const supportedBondListingTokens = [
     address: "0xfc5a1a6eb076a2c7ad06ed22c90d7e710e35ad0a",
   },
 ];
+
+export type SupportedToken = {
+  chainId: number;
+  name: string;
+  symbol: string;
+  address: string;
+};
