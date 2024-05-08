3ba01013e7bc076471c1c8e6978443dbd7891e66
garlic-brewlabs <88916999+garlic-brewlabs@users.noreply.github.com>
2024-05-08 23:27:32 +1000

    Adding vested bond to mock data

--- src/hooks/useBondPurchasedData.ts
+++ src/hooks/useBondPurchasedData.ts
@@ -31,7 +31,33 @@ export const useBondPurchasedData = (dataId: string) => {
         bondSalePrice: 0.005676,
         bondPurchaseDate: 1714560003255,
         bondMarketPrice: 0.0035467,
-
+        bondName: "BREWLABS/USDT",
+        bondClaimable: {
+          total: 1000,
+          remaining: 998,
+        },
+      },
+      {
+        bondChainId: 56,
+        bondType: "tokenVested",
+        bondToken: {
+          address: "0x6aac56305825f712fd44599e59f2ede51d42c3e7",
+          name: "Brewlabs",
+          symbol: "BREWLABS",
+          decimals: 18,
+          chainId: 56,
+        },
+        bondVestingPeriod: 100,
+        bondSaleToken: {
+          address: "0x55d398326f99059fF775485246999027B3197955",
+          name: "USDT",
+          symbol: "USDT",
+          decimals: 18,
+          chainId: 56,
+        },
+        bondSalePrice: 0.005676,
+        bondPurchaseDate: 1714560003255,
+        bondMarketPrice: 0.0035467,
         bondName: "BREWLABS/USDT",
         bondClaimable: {
           total: 1000,
